316e16c4b4d3b4b294cb9c5e66854d3b12e04e6a
katieslobodsky <katieslobodsky@gmail.com>
2025-10-13 16:12:56 -0700

    Added util.c

---
+++ util.c
@@ -0,0 +1,13 @@
+#include <signal.h>
+#include "util.h"
+
+void install_handler(int sig, void (*handler)(int)) {
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = handler;
+    sigaction(sig, &sa, NULL);
+}
+
+void SIGINT_handler(int sig)  { (void)sig; }
+void SIGQUIT_handler(int sig) { (void)sig; }
